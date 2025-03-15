e3d724f974eb01c9dcbb08c2ff43a26e277b3129
Samarth Kulkarni <samarth.kulkarni@joshsoftware.com>
2025-03-15 21:12:46 +0530

    [add]: try-out object examples

--- 2UnderstandingObjects/main.js
+++ 2UnderstandingObjects/main.js
@@ -2,6 +2,10 @@ const person = {
   name: "Bru",
   age: 25,
   "person hobbies": ["music", "sports"],
+  abac: {
+    k: "ksd",
+    v: "2345"
+  }
 }
 
 // for in loop
@@ -12,7 +16,24 @@ for(let key in person) {
   // console.log(`${key}: ${person[key]}`) // this will return: key_name: undefined
   console.log(person[key]); //this will iterate entire object `person`
 }
+
 */
+
+
+
+// for (k in person) {
+//   console.log(`${k} -> ${(person[k])}`)
+// }
+
+// console.log("----------------------")
+
+// console.log(JSON.stringify(person[abac]))
+// for (let k in person) {
+//   console.log(`${k} -> ${JSON.stringify(person[k])}`);
+// }
+
+
+// console.log("----------------------")
 
 // console.log(Object.keys(person)); // this will return an array of keys of the object `person`
 
@@ -33,36 +54,65 @@ console.log(obj);
 
 
 // Spread Operator
-const newArr = [..."newString"]
-console.log(newArr);
+// const newArr = [..."newString"]
+// console.log(newArr);
 
-// Spread operator in objects
-const newObj = {
-  ...["item1", "item2"]
+// // Spread operator in objects
+// const newObj = {
+//   ...["item1", "item2"]
+// }
+// console.log(newObj);
+
+// const newObj2withString = {
+//   ..."ThisIsNewString"
+// }
+// console.log(newObj2withString);
+
+// // Object destructuring
+// const band = {
+//   bandName: "Metallica",
+//   famousSong: "Enter Sandman",
+//   year: 1981,
+
+//   anotherFamousSong: "Nothing Else Matters"
+// };
+
+// // const bandName = band.bandName;
+// // const famousSong = band.famousSong;
+// // bandName = "Iron Maiden";
+
+// const { bandName: bN, famousSong: fS, ...restProps } = band;
+
+// console.log(`The band "${bN}" is famous for their song "${fS}"`);
+// console.log(bN);
+// console.log(fS);
+// console.log(restProps); //returns remaining properties of object `band`
+
+
+
+// ========================================================================================
+
+/*
+console.log(ab)
+getName()
+
+var ab = 35
+
+function getName() {
+  console.log("this is my name")
 }
-console.log(newObj);
 
-const newObj2withString = {
-  ..."ThisIsNewString"
+*/
+
+// ========================================================================================
+
+function a() {
+  function c() {
+    console.log(b)
+  }
+  c()
 }
-console.log(newObj2withString);
 
-// Object destructuring
-const band = {
-  bandName: "Metallica",
-  famousSong: "Enter Sandman",
-  year: 1981,
-
-  anotherFamousSong: "Nothing Else Matters"
-};
-
-// const bandName = band.bandName;
-// const famousSong = band.famousSong;
-// bandName = "Iron Maiden";
-
-const {bandName: bN, famousSong: fS, ...restProps} = band;
-
-console.log(`The band "${bN}" is famous for their song "${fS}"`);
-console.log(bN);
-console.log(fS);
-console.log(restProps); //returns remaining properties of object `band`
+const b = 243;
+a()
+// console.log(b)
